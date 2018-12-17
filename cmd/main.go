8adb62ba81ea4d9c134b6a4a7a729876c8759169
zhang liang <zhangliang@usechain.net>
2018-12-17 18:13:27 +0800

    add rpc tx sender

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"math/big"
+	"fmt"
+	"github.com/usechain/go-usedrpc"
+)
+
+func main() {
+	c := usedrpc.NewUseRPC("http://127.0.0.1:8545")
+	accounts, _ := c.UseAccounts()
+	tx := usedrpc.T {
+		From: accounts[0],
+		To:   "0xfffffffffffffffffffffffffffffffff0000001",
+		Value: big.NewInt(0),
+		Data:  "",
+	}
+	for {
+		flag, err := c.UseSendTransaction(tx)
+		fmt.Println("The tx hash:", err, flag)
+	}
+}
+
+
+
